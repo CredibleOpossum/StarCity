e322200502fd9c71b30deff035b63643bf19ed0a
IncrementalMan <63551903+IncrementalMan@users.noreply.github.com>
2020-05-07 22:52:51 +0200

    Delete update.js

--- update.js
+++
@@ -1,4 +0,0 @@
-function update() {
-    ElementById("currentWood").innerText = "Wood: " + game.resources.wood.toFixed(2)
-    ElementById("currentWheat").innerText = "Wheat: " + game.resources.wheat.toFixed(2)
-}
